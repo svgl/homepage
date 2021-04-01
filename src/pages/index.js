f675bb2a4212362591ced6d443d9be8a82c1ee63
svgl <github@vergeyle.net>
2021-04-01 13:04:22 -0500

    feat: added resume pdf

--- src/pages/index.js
+++ src/pages/index.js
@@ -7,10 +7,11 @@ const IndexStyles = styled.main`
   width: 100%;
   margin: 0 auto;
   padding: 0 1.5rem;
-  font-size: 1.1rem;
+  font-size: 1.125rem;
 
   h1 {
     margin: 1rem 0;
+    font-size: 1.25rem;
   }
 `;
 
@@ -20,6 +21,10 @@ export default function IndexPage() {
   return (
     <IndexStyles>
       <h1>Welcome! I am Seth Vergeyle</h1>
+      <p>
+        I am a Web Developer with 4+ years of experience creating internal web
+        applications.
+      </p>
       <button type="button" onClick={() => toggleDarkTheme()}>
         Change theme!
       </button>
--- src/utils/usePrefersDarkTheme.js
+++ src/utils/usePrefersDarkTheme.js
@@ -18,8 +18,6 @@ export function usePrefersDarkTheme() {
     const html = document.querySelector('html');
     const theme = prefersDarkTheme ? 'theme-dark' : 'theme-light';
 
-    console.log(`changing to theme ${theme}`);
-
     html.classList.remove(prefersDarkTheme ? 'theme-light' : 'theme-dark');
     html.classList.add(theme);
   }, [prefersDarkTheme]);
